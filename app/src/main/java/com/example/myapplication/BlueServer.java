131be985072fe7fe741a361d012fc86d0cdfa870
lowie spriet <lowie.spriet@gmail.com>
2019-05-08 11:32:07 +0200

    aanmaak service

---
+++ app/src/main/java/com/example/myapplication/BlueServer.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+public class BlueServer extends Service  {
+
+
+    public Activity myAct;
+
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    IBinder mBinder = new LocalBinder();
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+
+
+
+
+    public class LocalBinder extends Binder {
+        public BlueServer getServerInstance() {
+            return BlueServer.this;
+        }
+    }
+
+
+}
